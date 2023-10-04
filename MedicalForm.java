7b97d058b35b481a2537a76945b42afbe26fd12a
HannaMG <hanna.mendoza@fantasyeh.com>
2023-10-04 18:29:52 -0400

    MedicalForm added

---
+++ MedicalForm.java
@@ -0,0 +1,28 @@
+public class MedicalForm {
+    private String name;
+    private int dob;
+    private String address;
+    private String condition;
+    private int alienNumber;
+    private int medicalFormID;
+
+    public int CreateNewMedicalFormID(String name, int dob, String address, String condition, int alienNumber) {
+        this.name = name;
+        this.dob = dob;
+        this.address = address;
+        this.condition = condition;
+        this. alienNumber = alienNumber;
+
+        return medicalFormID;
+    }
+
+    public MedicalForm GetMedicalForm(int medicalFormID) {
+        //Validate id 
+        return this;
+    }
+
+    public ValidateMedicalForm() {
+
+    }
+
+}
