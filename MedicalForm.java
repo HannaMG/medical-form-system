beaac48da83d50b29cce9687cfc3f6fafa28f17e
HannaMG <hanna.mendoza@fantasyeh.com>
2023-10-16 14:20:27 -0400

    Updated Medical Form and enabled testing

--- MedicalForm.java
+++ MedicalForm.java
@@ -5,15 +5,23 @@ public class MedicalForm {
     private String condition;
     private int alienNumber;
     private int medicalFormID;
+    static private int nextID = 0;
 
-    public int CreateNewMedicalFormID(String name, int dob, String address, String condition, int alienNumber) {
-        this.name = name;
+    private MedicalForm(String name, int dob, String address, String condition, int alienNumber, int medicalFormID) {
+       this.name = name;
         this.dob = dob;
         this.address = address;
         this.condition = condition;
-        this. alienNumber = alienNumber;
+        this. alienNumber = alienNumber; 
+        this.medicalFormID = medicalFormID;
+    }
 
-        return medicalFormID;
+    static public int CreateNewMedicalFormID(String name, int dob, String address, String condition, int alienNumber) {
+        MedicalForm mf = new MedicalForm(name, dob, address, condition, alienNumber, nextID);
+
+        //Add mf to database
+
+        return  nextID++;
     }
 
     public MedicalForm GetMedicalForm(int medicalFormID) {
@@ -21,8 +29,8 @@ public class MedicalForm {
         return this;
     }
 
-    public ValidateMedicalForm() {
-
+    public boolean ValidateMedicalForm() {
+        return true; //Need to change ???
     }
 
 }
